14fd56a7306b927bcba620349246d5a0a4972987
NataR78 <100790826+NataR78@users.noreply.github.com>
2022-07-29 21:53:05 +0300

    myGame

---
+++ script.js
@@ -0,0 +1,19 @@
+let podLodka = document.getElementById("pod-lodka")
+let gamePole = document.getElementById("game-container")
+
+
+
+
+var xPos = 0;
+var yPos = 0;
+var angle = 0;
+
+function animate() {
+  
+  }
+
+  requestAnimationFrame(animate);
+
+animate();
+
+
