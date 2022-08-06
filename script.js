d2f2ce34cf211db04d97392692cbbc908ff39f00
NataManatu <vip.aleksa78@mail.ru>
2022-08-06 22:18:01 +0300

    myGame

--- script.js
+++ script.js
@@ -4,18 +4,7 @@ let gamePole = document.getElementById("game-container");
 var music = new Audio("audio.mp3")
 	music.play()
 
-    function go() {
-        if (times % 2) {
-          boat.classList.remove('back');
-          boat.style.marginLeft = 100 * times + 200 + 'px';
-        } else {
-          boat.classList.add('back');
-          boat.style.marginLeft = 100 * times - 200 + 'px';
-        }
-
-      }
-
-      go()
+  
 
 
 
