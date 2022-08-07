3318aeec1c492143a9c62c7fbdda4f2c45cd9696
NataManatu <vip.aleksa78@mail.ru>
2022-08-07 17:08:34 +0300

    myGame

--- script.js
+++ script.js
@@ -4,13 +4,32 @@ let podLodka = document.getElementById("pod-lodka");
 let music = new Audio("audio.mp3");
 music.play()
 
+function upMove(event) {
+
+		let code = event.keyCode || event.charCode;
+		if (code != 38 && code != 40)
+			return;
+		podLodka.style.top = podLodka.offsetLeft+ (code == 38 ? -70 : 70) + 'px';
+
+
+	// var y = 0;
+	// if (event.keyCode == 38) {
+	// 	y = y - 100;
+	// 	podLodka.style.top = y + 'px';
+	// }
+	// else if (event.keyCode == 40) {
+	// 	y = y + 100;
+	// 	podLodka.style.top = y + 'px';
+	// }
+
+}
+
+addEventListener("keydown", upMove)
 
 
 
-	
-	 
-	  
-	
+
+
 
 
 
