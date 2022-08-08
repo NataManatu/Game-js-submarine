7aea2e597faa02f5fbf1f2c7ced82c54acabb04f
NataManatu <vip.aleksa78@mail.ru>
2022-08-08 13:00:50 +0300

    myGame

--- script.js
+++ script.js
@@ -7,21 +7,30 @@ music.play()
 
 function upMove(event) {
 
-	var y = 0;
+	var y = 50;
 	if (event.keyCode == 87) {
 		y = y - 70;
 		podLodka.style.top = y + 'px';
 
 	}
-	else if (event.keyCode == 88) {
+	if (event.keyCode == 88) {
 		y = y + 100;
 		podLodka.style.top = y + 'px';
 
-	}
-
-	// alert( event.key)
+	}	// alert( event.key)
 }
 addEventListener("keydown", upMove)
+
+function MacroCollision(podLodka, fish_1) {
+	let XColl = false;
+	let YColl = false;
+
+	if ((podLodka.x + podLodka.width >= fish_1.x) && (podLodka.x <= fish_1.x + fish_1.width)) XColl = true;
+	if ((podLodka.y + podLodka.height >= fish_1.y) && (podLodka.y <= fish_1.y + fish_1.height)) YColl = true;
+
+	if (XColl & YColl) { return true; }
+	return false;
+}
 
 
 
