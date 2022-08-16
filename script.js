6d863befbfdf84698287a954c8ca022200313513
NataManatu <vip.aleksa78@mail.ru>
2022-08-16 15:34:15 +0300

    myGameSubmarine

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let gamePole = document.getElementById("game-container");
 let podLodka = document.getElementById("pod-lodka");
 let fish_1 = document.getElementById("fish1")
-let a = document.getElementById("parus-1")
-let b = document.getElementById("parus-2")
+let parus_1 = document.getElementById("parus-1")
+let parus_2 = document.getElementById("parus-2")
 
 
 let music = new Audio("audio.mp3");
@@ -22,8 +22,8 @@ function upMove(event) {
 	if (event.keyCode == 88) {
 		y = y + 100;
 		podLodka.style.top = y + 'px';
-		
-	}	
+
+	}
 }
 addEventListener("keydown", upMove)
 
