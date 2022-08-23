d3d08c4ce0bf5037a9ac02b22b5dc63bbcd95dab
NataManatu <vip.aleksa78@mail.ru>
2022-08-24 00:15:44 +0300

    myGame-submarine-Js

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ let fish_5 = document.getElementById("fish5");
 let anglerfish = document.getElementById("anglerfish");
 
 
-
 let y = 0;
+
+
 
 function move(event) {
 
@@ -36,60 +37,69 @@ addEventListener("keydown", move)
 
 let lst = document.querySelectorAll('.game-img');
 let gameEnded = false;
+let gameScore = 0
+
+function scoreMove() {
+	let score = document.getElementById("score")
+	score.innerHTML = `<p> ` + gameScore + `</p>`
+	score.style.display = "block"
+}
 
 lst.forEach((element) => {
 	console.log(element)
-	let gameScore = 0
+	
 	let interval = setInterval(() => {
 		let selection = podLodka;
 		let rectSelection = selection.getBoundingClientRect();
 		let rect = element.getBoundingClientRect();
 		let gameOver = document.getElementById("game-over");
 		gameOver.innerHTML = '<p>твой счет  ' + gameScore + '<br> Game over!</p>'
-
+		
 		if (rect.bottom > rectSelection.top
 			&& rect.right > rectSelection.left
 			&& rect.top < rectSelection.bottom
 			&& rect.left < rectSelection.right) {
 			gameScore -= 1
 			// console.log(gameScore)
-
-			//сделать в фунцию 
-			let score = document.getElementById("score")
-			score.innerHTML = `<p> ` + gameScore + `</p>`
-			score.style.display = "block"
+			//сделать в функцию 
+			scoreMove()
+			// alert(gameScore)
+			console.log(gameScore)
 		}
 
 
-		let rect_2 = fish_2.getBoundingClientRect()
-		if (rect_2.bottom > rectSelection.top
-			&& rect_2.right > rectSelection.left
-			&& rect_2.top < rectSelection.bottom
-			&& rect_2.left < rectSelection.right) {
-			gameScore += 2
-			let score_2 = document.getElementById("score-2")
-			score_2.style.display = "block"
-		}
+		// let rect_2 = fish_2.getBoundingClientRect()
+		// if (rect_2.bottom > rectSelection.top
+		// 	&& rect_2.right > rectSelection.left
+		// 	&& rect_2.top < rectSelection.bottom
+		// 	&& rect_2.left < rectSelection.right) {
+		// 	gameScore += 2
+		// 	// let score_2 = document.getElementById("score-2")
+		// 	// score_2.style.display = "block"
+		// 	scoreMove()
+		// }
 
-		let rect_3 = anglerfish.getBoundingClientRect()
-		if (rect_3.bottom > rectSelection.top
-			&& rect_3.right > rectSelection.left
-			&& rect_3.top < rectSelection.bottom
-			&& rect_3.left < rectSelection.right) {
-			gameScore -= 2
-			let score_3 = document.getElementById("score-3")
-			score_3.style.display = "block"
-		}
+		// let rect_3 = anglerfish.getBoundingClientRect()
+		// if (rect_3.bottom > rectSelection.top
+		// 	&& rect_3.right > rectSelection.left
+		// 	&& rect_3.top < rectSelection.bottom
+		// 	&& rect_3.left < rectSelection.right) {
+		// 	gameScore -= 2
+		// 	// let score_3 = document.getElementById("score-3")
+		// 	// score_3.style.display = "block"
+		// 	scoreMove()
+		// }
 
-		let rect_4 = parus_1.getBoundingClientRect()
-		if (rect_4.bottom > rectSelection.top
-			&& rect_4.right > rectSelection.left
-			&& rect_4.top < rectSelection.bottom
-			&& rect_4.left < rectSelection.right) {
-			gameScore -= 3
-			let score_4 = document.getElementById("score-4")
-			score_4.style.display = "block"
-		}
+		// let rect_4 = parus_1.getBoundingClientRect()
+		// if (rect_4.bottom > rectSelection.top
+		// 	&& rect_4.right > rectSelection.left
+		// 	&& rect_4.top < rectSelection.bottom
+		// 	&& rect_4.left < rectSelection.right) {
+		// 	gameScore -= 3
+		// 	// let score_4 = document.getElementById("score-4")
+		// 	// score_4.style.display = "block"
+		// 	scoreMove()
+		// }
 
 		if (gameScore <= -30) {
 			gameEnded = true
@@ -97,8 +107,8 @@ lst.forEach((element) => {
 			clearInterval(interval)
 			// alert("твой счет " + gameScore + " game over!")
 		}
-
-	}, 500);
+		
+	}, 1300);
 
 })
 
